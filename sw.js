ae0ee7272254041c27c10c2e42832879938598f8
nttc5 <nttc5@gmail.com>
2026-07-03 15:25:35 +0800

    Optimize: Move site management to Mission tab, add swipe-to-delete gesture, fix Service Worker caching

--- sw.js
+++ sw.js
@@ -1,42 +1,24 @@
-var CACHE = 'asr2-v1';
-var ASSETS = [
-  '/ASR2/',
-  '/ASR2/index.html',
-  '/ASR2/manifest.json',
-  '/ASR2/工作場地標記.PNG',
-  '/ASR2/受困者標記.PNG',
-  '/ASR2/快速清理標記RCM.PNG',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
-];
-
+// Force immediate install
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(ASSETS);
-    })
-  );
   self.skipWaiting();
 });
 
+// Clear all caches and unregister on activation
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+      return Promise.all(keys.map(function(k) {
+        return caches.delete(k);
+      }));
+    }).then(function() {
+      return self.registration.unregister();
+    }).then(function() {
+      console.log('Service Worker successfully unregistered and cache cleared.');
     })
   );
-  self.clients.claim();
 });
 
+// Intercept nothing, bypass cache completely
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(cached) {
-      return cached || fetch(e.request).then(function(resp) {
-        var clone = resp.clone();
-        caches.open(CACHE).then(function(cache){cache.put(e.request, clone);});
-        return resp;
-      });
-    }).catch(function() {
-      return caches.match('./index.html');
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
